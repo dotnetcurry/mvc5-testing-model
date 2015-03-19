ae2e6da37774251afef69503372bbb5888f9db42
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2015-03-19 07:27:23 +0530

    model testing code

---
+++ MVC5_Testing_1/ModelAccess/DepartmentAccess.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using MVC5_Testing_1.Models;
+
+namespace MVC5_Testing_1.ModelAccess
+{
+    /// <summary>
+    /// Interface for providing functnality for Department Data Access
+    /// </summary>
+    public interface IDepartmentAccess
+    {
+       List<Department> GetDepartments();
+       Department GetDepartment(int id);
+       void CreateDepartment(Department dept);
+       bool CheckDeptExist(int id);
+    }
+
+    /// <summary>
+    /// The Department Data Access
+    /// </summary>
+    public class DepartmentAccess : IDepartmentAccess
+    {
+        CompanyEntities ctx;
+        public DepartmentAccess()
+        {
+            ctx = new CompanyEntities(); 
+        }
+        /// <summary>
+        /// Get All Departments
+        /// </summary>
+        /// <returns></returns>
+        public List<Department> GetDepartments()
+        {
+            var depts = ctx.Departments.ToList();
+            return depts;
+        }
+        /// <summary>
+        /// Get Department base on Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Department GetDepartment(int id)
+        {
+            var dept = ctx.Departments.Find(id);
+            return dept;
+        }
+        /// <summary>
+        /// Create Department
+        /// </summary>
+        /// <param name="dept"></param>
+        public void CreateDepartment(Department dept)
+        {
+            ctx.Departments.Add(dept);
+            ctx.SaveChanges();
+        }
+        /// <summary>
+        /// Check whether Department Exist or Not
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool CheckDeptExist(int id)
+        {
+            var dept = ctx.Departments.Find(id);
+            if (dept != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MVCModelTest/CheckDepartmentTest.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using MVC5_Testing_1.ModelAccess;
+using NUnit.Framework;
+
+namespace MVCModelTest
+{
+    [TestFixture]
+    public class CheckDepartmentTest
+    {
+        [Test]
+        public void CheckDepartmentExist()
+        {
+            var obj = new DepartmentAccess();
+
+            var Res = obj.CheckDeptExist(10);
+
+            Assert.That(Res, Is.True);
+        }
+
+        [Test]
+        public void CheckDepartmentExistWithMoq()
+        {
+            //Create Fake Object
+            var fakeObject = new Mock<IDepartmentAccess>();
+            //Set the Mock Configuration
+            //The CheckDeptExist() method is call is set with the Integer parameter type
+            //The Configuration also defines the Return type from the method  
+            fakeObject.Setup(x => x.CheckDeptExist(It.IsAny<int>())).Returns(true);
+            //Call the methid
+            var Res = fakeObject.Object.CheckDeptExist(10);
+
+            Assert.That(Res,Is.True);
+        }
+    }
+}
